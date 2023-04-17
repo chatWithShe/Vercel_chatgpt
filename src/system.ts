c9df3447d602706188a4a9b466088348741f4ae1
chatWithShe <130737867+chatWithShe@users.noreply.github.com>
2023-04-17 10:06:14 +0800

    更新说明信息

--- src/system.ts
+++ src/system.ts
@@ -10,9 +10,9 @@ export const defaultSetting = {
   model: "gpt-3.5-turbo" as Model
 }
 
-export const defaultMessage = `Powered by OpenAI Vercel
-- 如果本项目对你有所帮助，可以给小猫 [买点零食](https://cdn.jsdelivr.net/gh/ourongxing/chatgpt-vercel/assets/reward.gif)，但不接受任何付费功能请求。
-- 本网站仅作为项目演示，不提供服务，请填入自己的 Key。
+export const defaultMessage = `Powered by OpenAI 
+- 如果本项目对你有所帮助，可以给小猫 [买点零食](https://img.qzone.work:8443/2023/04/17/b39d7626b24fa.jpg)，我的QQ:2253018152。
+- 本网站仅作为项目演示，不提供服务，请点击输入框左侧设置图标填入自己的 OpenAI Key（SK）。[购买自己的chatGPT独享账号,带SK](https://ka.chatgpt12345.xyz)。
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof defaultSetting
